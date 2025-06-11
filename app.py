12e7d79ea2db6580b1b317e2da894be16d05e387
Dhilak-sybrant <147803098+Dhilak-sybrant@users.noreply.github.com>
2025-06-11 12:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,6 +176,8 @@ def home():
 def health():
     return {'status': 'healthy'}
 
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(debug=True, host="0.0.0.0", port=port)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
